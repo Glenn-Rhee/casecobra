54db57604277d51900c167d6c70f081af35f2c2c
Glenn-Rhee <glennviktor5@gmail.com>
2024-05-28 22:36:53 +0700

    feat: create testimonial upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import BodyCustomers from "@/components/Hero/BodyCustomers";
 import MainLeft from "@/components/Hero/MainLeft";
 import MainRight from "@/components/Hero/MainRight";
-import TitleCustomers from "@/components/Hero/TitleCustomers";
+import Phone from "@/components/Hero/Phone";
+import TitleCustomers, { Title } from "@/components/Hero/TitleCustomers";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Reviews from "@/components/Review/Reviews";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -14,14 +16,53 @@ export default function Home() {
           <MainRight />
         </MaxWidthWrapper>
       </section>
+
       <section className="bg-slate-100 py-24">
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <TitleCustomers />
           <BodyCustomers />
         </MaxWidthWrapper>
+
         <div className="pt-16">
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <Title
+                firstSentence="Upload your photo and get "
+                spanSentence="your own case"
+                lastSentence=" now"
+              />
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <Image
+                alt="Image Arrow"
+                src="/arrow.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+                width={126}
+                height={31}
+              />
+
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bggray-900/5 justify-center flex ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <Image
+                  alt="Image horse"
+                  src="/raya-try.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 w-auto h-auto"
+                  width={900}
+                  height={1600}
+                />
+              </div>
+              <Phone className="w-60 relative" imgsrc="/raya-try.jpg" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
       </section>
     </div>
   );
--- src/components/Hero/TitleCustomers.tsx
+++ src/components/Hero/TitleCustomers.tsx
@@ -1,17 +1,43 @@
 import Image from "next/image";
 import { Icons } from "../icons/Icons";
+import { cn } from "@/lib/utils";
+
+interface TitleProps {
+  firstSentence: string;
+  spanSentence: string;
+  lastSentence: string;
+  children?: React.ReactNode;
+}
+
+export function Title(props: TitleProps) {
+  const { firstSentence, spanSentence, lastSentence, children } = props;
+
+  return (
+    <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900 ">
+      {firstSentence}
+      <span
+        className={cn("relative px-2", {
+          "bg-green-600 text-white": !children,
+        })}
+      >
+        {spanSentence}
+        {children}
+      </span>{" "}
+      {lastSentence}
+    </h2>
+  );
+}
 
 export default function TitleCustomers() {
   return (
     <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
-      <h2 className="order- mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900 ">
-        What our{" "}
-        <span className="relative px-2">
-          customors{" "}
-          <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
-        </span>{" "}
-        say
-      </h2>
+      <Title
+        firstSentence="What our "
+        spanSentence="customers"
+        lastSentence=" say"
+      >
+        <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+      </Title>
       <Image
         src={"/snake-2.png"}
         className="w-24 order-first lg:order-2"
